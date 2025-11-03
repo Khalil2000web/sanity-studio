cd9d1f5d9f47045e26db8a860129a0011c74b04a
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-11-03 19:58:13 +0200

    Add schema definition for 'post' document

---
+++ schemas/schema.js
@@ -0,0 +1,11 @@
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({name: 'title', title: 'Title', type: 'string'}),
+    defineField({name: 'body', title: 'Body', type: 'text'})
+  ]
+})
