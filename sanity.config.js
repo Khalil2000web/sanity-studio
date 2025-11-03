c9ca0dcac4b13d3ef106f44ee6afc5889c5be975
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-11-03 19:57:31 +0200

    Create sanity.config.js

---
+++ sanity.config.js
@@ -0,0 +1,15 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import schema from './schemas/schema'
+
+export default defineConfig({
+  name: 'default',
+  title: 'My Sanity Studio',
+  projectId: 'tznuew2h',
+  dataset: 'production',
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: [schema],
+  },
+})
